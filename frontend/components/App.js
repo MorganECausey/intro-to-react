2d0b1f6fcd24cff9441a57d4d71ce2a2827c5709
Morgan Causey <morganelizabethcausey@gmail.com>
2023-08-30 16:43:54 -0400

    8/30 am commit

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,7 +1,6 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
-import Character from './Character'
-import { useState } from 'react' 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Character from './Character'; 
 
 const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
@@ -10,30 +9,26 @@ function App() {
   // ❗ Create state to hold the data from the API
   
   // ❗ Create effects to fetch the data and put it in state
-  const [urlPeople, setUrlPeople] = useState([]);
-  const [urlPlanets, setUrlPlanets] = useState([]);
+  const [peopleData, setPeopleData] = useState([]);
+  const [planetsData, setPlanetsData] = useState([]);
   const [selectedCharacter, setSelectedCharacter] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:9009/api/people')
-    .then(response => response.json())
-    .then(apiData => setUrlPeople(apiData))
+    axios.get(urlPeople)
+    .then(response => setPeopleData(response.data))
     .catch(error => console.error('Error fetching data', error));
-  })
-  
-  useEffect(() => {
-    fetch('http://localhost:9009/api/planets')
-      .then(response => response.json())
-      .then(apiData => setUrlPlanets(apiData))
+
+    axios.get(urlPlanets)
+      .then(response => setPlanetsData(response.data))
       .catch(error => console.error('Error fetching data', error));
   }, []);
 
   const handleCharacterClick = character => {
-    setSelectedCharacter(pervSelected => (pervSelected === character ? null : character))
-  }
+    setSelectedCharacter(prevSelected => (prevSelected === character ? null : character))
+  };
 
   const getPlanetByID = planetId => {
-    const planet = urlPlanets.find(planet => planet.id === planetId);
+    const planet = planetsData.find(planet => planet.id === planetId);
     return planet ? planet.name : 'Unknown';
   }
   return (
@@ -41,7 +36,7 @@ function App() {
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       <ul> 
-      {urlPeople.map(person => (
+      {peopleData.map(person => (
           <Character
           key={person.id}
           name={person.name}
@@ -52,8 +47,8 @@ function App() {
         ))}
         </ul>
     </div>
-  )
-};
+  );
+}
 /* ❗ Map over the data in state, rendering a Character at each iteration */
 export default App
 
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,25 +1,26 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState } from 'react'
 
 function Character(props) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
-  const [selectedCharacter, setSelectedCharacter] = useState(null)
   const { name, homeworld, isSelected, onClick } = props;
+  const [showHomeworld, setShowHomeworld] = useState(false);
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const handleHomeworldToggle = () => {
+    setShowHomeworld(prevShow => !prevShow);
+  };
 
   return (
-    <div>
-      <h1>Characters </h1>
-      <li
+      <div
       className={`character-card ${isSelected ? 'selected' : ''}`}
       onClick={onClick}
       >
-        <p className="character-name">{name} (Person)</p>
-        {isSelected && (
-          <p className="character-planet">Homeworld {homeworld}</p>
+        <div>
+          <p className="character-name" onClick={handleHomeworldToggle}>{name}</p>
+        {isSelected && showHomeworld && (
+          <p className="character-planet">Planet: {homeworld}</p>
         )}
-      </li>
-      {/* Use the same markup with the same attributes as in the mock */}
-    </div>
+        </div>
+      </div>
   );
 }
 
